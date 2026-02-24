28d27fe50c842d3b8698cd32e52334a0fb48b0ea
William Zhang <williamb.zhang@outlook.com>
2026-02-24 03:50:30 -0500

    flowers

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 onload = () => {
   document.body.classList.remove("container");
+
+  const flowers = document.querySelectorAll(".flower");
+
+  flowers.forEach((flower, index) => {
+    flower.style.cursor = "pointer";
+
+    flower.addEventListener("click", () => {
+      window.location.href = `page${index + 1}.html`;
+    });
+  });
 };
